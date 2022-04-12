9da033d0cce52c77f34e284c423209f27ac2c4b0
Rexter9910 <101357846+Rexter9910@users.noreply.github.com>
2022-04-12 18:37:13 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+
+function preload(){
+  //pre-load images
+  path = loadImage("path.png")
+}
+
+function setup(){
+  createCanvas(400,400);
+  //create sprites here
+}
+
+function draw() {
+  background(0);
+
+}
